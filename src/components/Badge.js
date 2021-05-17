8887ef8755a3c4d1e2f34bf7a3a1692b8a6f1429
Cristian <llcristianaceroll@gmail.com>
2021-05-17 16:05:51 -0500

    aplicacion de estilos

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,26 +1,26 @@
 import React from 'react';
+import './styles/Badge.css';
 import confLogo from '../images/badge-header.svg';
+
 
 class Badge extends React.Component {
     render() {
         return (
-            <div>
-                <div>
+            <div className="Badge">
+                <div className="Badge__header">
                     <img src={confLogo} alt="Logo de la conferencia" />                    
                 </div>
 
-                <div>
-                    <img src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200
-
-" alt= "Avatar"/>
+                <div className="Badge__section-name">
+                    <img  className="Badge__avatar" src="https://media-exp1.licdn.com/dms/image/C4E03AQGlasG70LC_kg/profile-displayphoto-shrink_800_800/0/1619050770031?e=1626912000&v=beta&t=xxIbqsmApJFV__9zpcQ8jknvamXMv-Olp7XmHeoOhf8" alt= "Avatar"/>
                     <h1>Cristian <br/> Acero</h1>
                 </div>
-                <div>
-                    <p>Frontend Engineer</p>
-                    <p>@llcristianll</p>
+                <div className="Badge__section-info">
+                    <h3>Frontend Engineer</h3>
+                    <div>@llcristianll</div>
                 </div>
 
-                <div>
+                <div className="Badge__footer">
                     #platziconf
                 </div>
             </div>
