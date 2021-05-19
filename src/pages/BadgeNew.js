b85bff31531febaa9b497159e064b728f56a2324
Cristian <llcristianaceroll@gmail.com>
2021-05-19 11:55:07 -0500

    Enlazando eventos> conectar la accionde usario con los componente sde ract

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import './styles/BadgeNew.css'
-import header from '../images/badge-header.svg'
-import Navbar from '../components/Navbar';
+import './styles/BadgeNew.css';
+import header from '../images/badge-header.svg';
 import Badge from '../components/Badge';
+import BadgeForm from '../components/BadgeForm';
+import Navbar from '../components/Navbar';
 
 
 class BadgeNew extends React.Component {
@@ -21,6 +22,9 @@ class BadgeNew extends React.Component {
                                 avatarUrl= "https://media-exp1.licdn.com/dms/image/C4E03AQGlasG70LC_kg/profile-displayphoto-shrink_800_800/0/1619050770031?e=1626912000&v=beta&t=xxIbqsmApJFV__9zpcQ8jknvamXMv-Olp7XmHeoOhf8"
                                 jobTitle="Frontend Engineer"
                                 twitter="lcristianl"/>
+                        </div>
+                        <div className="col-6">
+                            <BadgeForm />
                         </div>
                     </div>
                 </div>
